d0623f077dbe31fef4399fac0d818d8eacb1cce2
the-somvanshi <33524968+the-somvanshi@users.noreply.github.com>
2019-01-26 05:10:16 +0530

    Add files via upload
    90

--- newral2.py
+++ newral2.py
@@ -1,11 +1,8 @@
 import numpy as np
-input=[2,2,7,4,5,7,5,8]
-target=[4,1,3]
-hiden=[]
-weight=[]
-biase=[]
+import copy
+
 ######################################
-def network(input,output,nofhiden=2,nodeinhiden=4):
+def network(input,output,nofhiden=2,nodeinhiden=2):
     global biase
     wt=[]
     biase=np.random.rand(nofhiden+1)
@@ -25,7 +22,6 @@ def network(input,output,nofhiden=2,nodeinhiden=4):
 ######################################
 
 def feedforword(input,w,biase):
-    #print(w)
     s=(np.matrix(w)).size
     out=s/len(input)
     h=[]
@@ -47,13 +43,20 @@ def sigmoid(x):
     return a
 ######################################
 def errwt(alpha,out):
+    alpha.reverse()
+    out.insert(0,input)
     cw=[]
-    e=alpha.pop()*out.pop()
-    cw.append(e)
+    out.pop()
+    for i in range(len(out)):
+        e=np.matrix(alpha.pop()).transpose()*out.pop()
+        cw.append(e)
     return cw
 #####################################
 def err1(input,output,target,w):
     wt=w
+    wz=w
+    tz=target
+    print(len(wz))
     out=output
     z=[]
     alpha=[]
@@ -61,64 +64,58 @@ def err1(input,output,target,w):
     for i in range(len(target)):
         dif=output[len(output)-1][i]-target[i]
         der=output[len(output)-1][i]*(1-output[len(output)-1][i])
-        z=alpha.insert(0,dif*der)
-        salpha.append(dif*der)
-    print("alpha isz"+str(alpha))
-    out.insert(0,input)
+        mul=dif*der
+        alpha.insert(0,mul)
+        z.append(mul)
+    salpha.append(z)
     o=out.pop()
     o=out.pop()
     for i in range(len(w)-1):
         temp=[]
-        print("alpha in loop"+str(alpha)+"shape"+str(np.array(alpha).shape))
         s=np.matrix(alpha)*np.matrix(wt.pop())
-        print("s"+str(s))
         for i in range(len(s)):
             b=s[i]*o[i]*(1-o[i])
             temp.append(b)
             salpha.append(np.array(b))
-            print("@"+str(b))
+        temp=np.array(temp).reshape(s.size,)
         alpha.clear()
-        print("weight1 isz"+str(temp))
-        temp=np.array(temp).reshape(4,)
         alpha.append(temp)
-        print("new alpha"+str(alpha))
-        
     return salpha
-
-    
-#####################################
-def error(output,target,outh):
-    #$etotal/output----output is last node of output--etotal is total error of network
-    etWo=output-target
-    print("diff"+str((etWo/target)*100)+"%")
-    #$output/$net-----net is without sigmoid value---output is sigmoid value
-    oWn=output*(1-output)
-    #$net/$wieght-----weight is weight that come to node for which we are calculating error
-    nWw=outh
-    #etotal/weight
-    return (etWo*oWn*nWw)
 #####################################
 def update(wt,err,eta=.5):
-    return(wt-eta*err)
+    print(err)
+    a=[]
+    for x in err:
+        z=x*eta
+        a.append(z)
+    #print(a)
+    d=np.subtract(wt,a)
+   # print(d)
+    return(d)
  #######################################
-w=network(input,len(target))
-print("biasae"+str(biase))
-neth=[]
-outh=[]
 def bp(input):
     for k in range(len(w)):
         net=feedforword(input,w[k],biase[k])
-       # print("net"+str(net))
         out=sigmoid(net)
-       # print("out"+str(out))
         neth.append(net)
         outh.append(out)
         input=out
-   # print("neth"+str(neth))
-   # print("out"+str(outh))
-
+#####################################
+input=[2,2]
+target=[4,1]
+hiden=[]
+weight=[]
+biase=[]
+neth=[]
+outh=[]
+w=network(input,len(target))
+print("weight"+str(w))
 bp(input)
-e=err1(input,outh,target,w)
-#z=errwt(e,outh)
-#print("sende"+str(z))
-print(len(e))
+nout=copy.deepcopy(outh)
+gw=copy.deepcopy(w)
+e=err1(input,nout,target,gw)
+z=errwt(e,outh)
+nw=update(w,z)
+print(nw)
+w=nw
+
